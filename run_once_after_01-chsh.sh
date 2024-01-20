85044fbe74e4ebe35e90ed04a4245322388ee624
kirici <74684021+kirici@users.noreply.github.com>
2024-01-20 16:34:22 +0100

    chez: do not attempt to run chsh, print info;
    It is a hassle to handle this noninteractively while portable

--- run_once_after_01-chsh.sh
+++ run_once_after_01-chsh.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-chsh -s "$(which zsh)"
+log_info() {
+    /bin/echo -e "\e[104m\e[97m[INFO]\e[49m\e[39m ${*}"
+}
+
+clear
+log_info "To change your default shell, run:"
+# shellcheck disable=SC2016
+log_info 'chsh -s "$(which zsh)"'
