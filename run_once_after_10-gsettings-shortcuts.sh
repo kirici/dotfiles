9acf5d06fd7a720f7fc6f97f7f4f8588d6e238ff
kirici <74684021+kirici@users.noreply.github.com>
2024-01-20 18:21:17 +0100

    chez: gsettings for shortcuts added

---
+++ run_once_after_10-gsettings-shortcuts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>t'"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'gnome-terminal'"
