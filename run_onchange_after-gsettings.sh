a508cf486b3599642565fffd1432dba614e0978c
kirici <74684021+kirici@users.noreply.github.com>
2024-01-21 17:14:07 +0100

    chez(gsettings): terminal prefs added

--- run_onchange_after-gsettings.sh
+++ run_onchange_after-gsettings.sh
@@ -15,6 +15,27 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono NL
 gsettings set org.gnome.shell enabled-extensions "['background-logo@fedorahosted.org', 'Vitals@CoreCoding.com', 'clipboard-history@alexsaveau.dev']"
 gsettings set org.gnome.shell favorite-apps "['com.github.Eloston.UngoogledChromium.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
 
+# Terminal
+profile=$(gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}')
+gsettings_list=(
+    'audible-bell false'
+    'bold-color-same-as-fg true'
+    'bold-is-bright true'
+    'cursor-shape '"'block'"''
+    'default-size-columns 150'
+    'default-size-rows 50'
+    'scrollback-lines 10000'
+    'use-system-font true'
+    'use-theme-colors true'
+    'background-transparency-percent 5'
+    'use-transparent-background true'
+    'visible-name '"'brk'"''
+)
+for setting in "${gsettings_list[@]}"; do
+    # shellcheck disable=SC2086
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"${profile}"/ ${setting}
+done
+
 # Shortcuts
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
