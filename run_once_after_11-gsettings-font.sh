e5dcf57b5c97898ddb63f39993b979ab68cfd5e0
kirici <74684021+kirici@users.noreply.github.com>
2024-01-21 17:11:36 +0100

    chez: consolidate gsettings scripts

--- run_once_after_11-gsettings-font.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono NL 12'
--- run_onchange_after-gsettings.sh
+++ run_onchange_after-gsettings.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -euo pipefail
 
+if ! command -v gsettings >/dev/null 2>&1; then
+    echo "No gsettings found"
+    exit 1
+fi
+fc-cache -f
+
+# Gnome desktop interface
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono NL 12'
+
+# Shortcuts
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'/usr/bin/gnome-terminal'"
