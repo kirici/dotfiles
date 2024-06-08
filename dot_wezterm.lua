a141c53a70e9873c3df221394434599f3174f785
kirici <74684021+kirici@users.noreply.github.com>
2024-06-08 12:50:41 +0200

    style(wezterm): stylua formatting

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,24 +1,24 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- CONFIG START
 
--- config.color_scheme = 'Elementary (Gogh)'
--- config.color_scheme = 'Espresso (Gogh)'
-config.color_scheme = 'Obsidian (Gogh)'
+-- config.color_scheme = "Elementary (Gogh)"
+-- config.color_scheme = "Espresso (Gogh)"
+config.color_scheme = "Obsidian (Gogh)"
 
 config.initial_rows = 35
 config.initial_cols = 140
 config.hide_tab_bar_if_only_one_tab = true
 
--- config.font = wezterm.font 'JetBrainsMonoNL'
-config.font = wezterm.font 'JetBrainsMonoNL Nerd Font'
+-- config.font = wezterm.font("JetBrainsMonoNL")
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 13.0
 
 config.window_frame = {
-  font_size = 13.0,
-  -- active_titlebar_bg = '#333333',
-  -- inactive_titlebar_bg = '#333333',
+	font_size = 13.0,
+	-- active_titlebar_bg = "#333333",
+	-- inactive_titlebar_bg = "#333333",
 }
 
 -- CONFIG END
