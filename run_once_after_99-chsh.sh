d3df4752c261bf7fb10a1fc9100f17b885e7b71a
kirici <74684021+kirici@users.noreply.github.com>
2024-07-21 14:18:04 +0200

    docs: do not clear terminal output at the end

--- run_once_after_99-chsh.sh
+++ run_once_after_99-chsh.sh
@@ -4,7 +4,6 @@ log_info() {
     /bin/echo -e "\e[104m\e[97m[INFO]\e[49m\e[39m ${*}"
 }
 
-clear
 log_info "To change your default shell, run:"
 # shellcheck disable=SC2016
 log_info 'chsh -s "$(which zsh)"'
