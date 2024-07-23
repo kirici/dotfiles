e5fc443165a0e63dafd608f5d964373cc0c25bb0
kirici <74684021+kirici@users.noreply.github.com>
2024-07-24 01:17:02 +0200

    chore(astro): migrate to v4 api

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,15 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "go",
-      "just",
-      "lua",
-      "python",
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"go",
+			"just",
+			"lua",
+			"python",
+			-- add more arguments for adding more treesitter parsers
+		},
+	},
 }
