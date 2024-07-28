b04fc5a38c90e19e9d111f7d6dfe842f893b1a11
kirici <74684021+kirici@users.noreply.github.com>
2024-07-28 13:33:06 +0200

    feat(wezterm): thinner border, hex codes for colors

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -17,15 +17,15 @@ config.font_size = 13.0
 
 config.window_frame = {
 	font_size = 13.0,
-	inactive_titlebar_bg = "grey",
-	border_left_width = "0.5cell",
-	border_right_width = "0.5cell",
-	border_bottom_height = "0.25cell",
-	border_top_height = "0cell",
-	border_left_color = "grey",
-	border_right_color = "grey",
-	border_bottom_color = "grey",
-	border_top_color = "grey",
+	inactive_titlebar_bg = "#808080",
+	border_left_width = "0.2cell",
+	border_right_width = "0.2cell",
+	border_bottom_height = "0.1cell",
+	border_top_height = "0.1cell",
+	border_left_color = "#313131",
+	border_right_color = "#313131",
+	border_bottom_color = "#313131",
+	border_top_color = "#313131",
 }
 
 -- CONFIG END
