2b3e554e330f4020a929708bcc6124779757ed77
kirici <74684021+kirici@users.noreply.github.com>
2024-11-02 10:07:35 +0100

    feat(gset)!: remove settings for deprecated gnome terminal

--- run_onchange_after-gsettings.sh
+++ run_onchange_after-gsettings.sh
@@ -19,27 +19,6 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMonoNL N
 gsettings set org.gnome.shell enabled-extensions "['background-logo@fedorahosted.org', 'Vitals@CoreCoding.com', 'clipboard-history@alexsaveau.dev', 'pano@elhan.io', 'launch-new-instance@gnome-shell-extensions.gcampax.github.com', 'search-light@icedman.github.com', 'tactile@lundal.io']"
 gsettings set org.gnome.shell favorite-apps "['io.github.ungoogled_software.ungoogled_chromium.desktop', 'code.desktop', 'kitty.desktop', 'org.gnome.Nautilus.desktop']"
 
-# Terminal
-profile=$(gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}')
-gsettings_list=(
-    'audible-bell false'
-    'bold-color-same-as-fg true'
-    'bold-is-bright true'
-    'cursor-shape '"'block'"''
-    'default-size-columns 140'
-    'default-size-rows 35'
-    'scrollback-lines 10000'
-    'use-system-font true'
-    'use-theme-colors true'
-    'background-transparency-percent 0'
-    'use-transparent-background true'
-    'visible-name '"'brk'"''
-)
-for setting in "${gsettings_list[@]}"; do
-    # shellcheck disable=SC2086
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:"${profile}"/ ${setting}
-done
-
 # Shortcuts
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
