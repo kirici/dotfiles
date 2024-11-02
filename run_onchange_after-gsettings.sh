38a27cdfde0440032a880a982e0aa7a26b92e575
kirici <74684021+kirici@users.noreply.github.com>
2024-11-02 10:59:08 +0100

    feat(gset): minimal config for ptyxis

--- run_onchange_after-gsettings.sh
+++ run_onchange_after-gsettings.sh
@@ -14,8 +14,6 @@ fc-cache -f
 # Gnome desktop interface
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMonoNL Nerd Font 13'
-
-# Gnome shell
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Ptyxis.desktop', 'org.gnome.Nautilus.desktop']"
 
 # Shortcuts
@@ -23,3 +21,12 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Terminal'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'/usr/bin/ptyxis --new-window'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Super>t'"
+
+# Settings
+gsettings set org.gnome.Ptyxis visual-bell false
+gsettings set org.gnome.Ptyxis restore-session false
+gsettings set org.gnome.Ptyxis restore-window-size false
+gsettings set org.gnome.Ptyxis default-columns "uint32 120"
+gsettings set org.gnome.Ptyxis default-rows "uint32 24"
+gsettings set org.gnome.Ptyxis use-system-font true
+dconf write /org/gnome/Ptyxis/Profiles/"$(gsettings get org.gnome.Ptyxis profile-uuids | grep -oE '\w+')"/palette "'Darkside'"
