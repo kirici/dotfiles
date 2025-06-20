6520465c62e335cd9048b8b60c716a9f7649813f
kirici <74684021+kirici@users.noreply.github.com>
2025-06-20 09:37:22 +0200

    feat(shell): reduce env var scope
    Universal is mainly intended for fish-specific variables, as they
    become effective immediately, are shared across sessions and will
    be peristed until explicitly removed.

--- dot_config/private_fish/conf.d/envs.fish
+++ dot_config/private_fish/conf.d/envs.fish
@@ -1,16 +1,16 @@
-set -Ux EDITOR "nvim"
-set -Ux PAGER "less -FRX"
-set -Ux GIT_CONFIG_GLOBAL $XDG_CONFIG_HOME/git/.gitconfig
+set -gx EDITOR "nvim"
+set -gx PAGER "less -FRX"
+set -gx GIT_CONFIG_GLOBAL $XDG_CONFIG_HOME/git/.gitconfig
 
-set -Ux XDG_CONFIG_HOME $HOME/.config
-set -Ux XDG_CACHE_HOME $HOME/.cache
-set -Ux XDG_DATA_HOME $HOME/.local/share
-set -Ux XDG_STATE_HOME $HOME/.local/state
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 
-set -Ux GOPATH $XDG_DATA_HOME/go
-set -Ux GOMODCACHE $XDG_CACHE_HOME/go/pkg/mod
+set -gx GOPATH $XDG_DATA_HOME/go
+set -gx GOMODCACHE $XDG_CACHE_HOME/go/pkg/mod
 
 # See https://github.com/eza-community/eza/blob/main/man/eza_colors.5.md
-set -Ux EZA_COLORS "uu=0:gu=0:uR=31:gR=31:da=38;5;248:oc=0"
+set -gx EZA_COLORS "uu=0:gu=0:uR=31:gR=31:da=38;5;248:oc=0"
 
-set -Ux KUBECOLOR_OBJ_FRESH "8h"
+set -gx KUBECOLOR_OBJ_FRESH "8h"
