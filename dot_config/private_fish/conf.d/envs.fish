5242da7bf6440a7f97623106a1431facc38ed5a5
Burak Kirici <74684021+kirici@users.noreply.github.com>
2025-08-30 15:06:12 +0200

    feat(shell): add general purpose convenience vars

--- dot_config/private_fish/conf.d/envs.fish
+++ dot_config/private_fish/conf.d/envs.fish
@@ -10,6 +10,9 @@ set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx GOMODCACHE $XDG_CACHE_HOME/go/pkg/mod
 
+set -gx DESKTOP $HOME/Desktop
+set -gx DOWNLOADS $HOME/Downloads/
+
 # See https://github.com/eza-community/eza/blob/main/man/eza_colors.5.md
 set -gx EZA_COLORS "uu=0:gu=0:uR=31:gR=31:da=38;5;248:oc=0"
 
