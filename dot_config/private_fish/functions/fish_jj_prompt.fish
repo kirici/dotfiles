8c55ea5ac365f00a2870540a9081f089c85ceb92
kirici <74684021+kirici@users.noreply.github.com>
2025-09-12 16:20:44 +0200

    feat(shell): add jj prompt

---
+++ dot_config/private_fish/functions/fish_jj_prompt.fish
@@ -0,0 +1,42 @@
+# Adjusted from: https://gist.githubusercontent.com/camtheman256/028aa2f1ced68cd435093a2d4680cf88/raw/7363b64c197c33c1d0ce2b9154b0b32dd2a9ee5f/fish_jj_prompt.fish
+# Place me in ~/.config/fish/functions
+# Then add me to `fish_vcs_prompt`: `funced fish_vcs_prompt` and save it to
+# your personal config: `funcsave fish_vcs_prompt;`
+
+function fish_jj_prompt --description 'Write out the jj prompt'
+    # Is jj installed?
+    if not command -sq jj
+        return 1
+    end
+
+    # Are we in a jj repo?
+    if not jj root --quiet &>/dev/null
+        return 1
+    end
+
+    # Generate prompt
+    jj log --ignore-working-copy --no-graph --color always -r @ -T '
+        separate(
+            " ",
+            bookmarks.map(|c| c.name().substr(0, 16)).join(", "),
+            coalesce(
+                surround(
+                    "\"",
+                    "\"",
+                    if(
+                        description.first_line().substr(0, 24).starts_with(description.first_line()),
+                        description.first_line().substr(0, 24),
+                        description.first_line().substr(0, 23) ++ "…"
+                    )
+                ),
+                label(if(empty, "empty"), description_placeholder)
+            ),
+            change_id.shortest(),
+            commit_id.shortest(),
+            if(conflict, label("conflict", "(conflict)")),
+            if(empty, label("empty", "(empty)")),
+            if(divergent, "(divergent)"),
+            if(hidden, "(hidden)"),
+        )
+    '
+end
