896a1e32ac7bbb4eaee644ed15e937b50bf2d9d1
kirici <74684021+kirici@users.noreply.github.com>
2025-10-22 13:12:48 +0200

    feat(shell): add openstack status function for fish, add to right prompt
    Mimics kubectx.

--- dot_config/private_fish/functions/fish_right_prompt.fish
+++ dot_config/private_fish/functions/fish_right_prompt.fish
@@ -18,14 +18,21 @@ function kubectl_status
   set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
   [ -z $ns ]; and set -l ns 'default'
 
-  echo "("(set_color blue)"$ns"(set_color white)"$KUBECTL_PROMPT_SEPARATOR$ctx)"
+  echo (set_color blue)⎈" "(set_color brblue)$ns(set_color white)$KUBECTL_PROMPT_SEPARATOR(set_color blue)$ctx(set_color normal)
+end
+
+function openstack_status
+  set -l os_cloud $OS_CLOUD
+  set -l os_region (yq ".clouds.$os_cloud.region_name" ~/.config/openstack/clouds.yaml 2>/dev/null)
+  [ -z $os_region ]; and set -l os_region '-'
+  echo (set_color red)⛶" "(set_color brred)$OS_CLOUD(set_color white).(set_color red)$os_region(set_color normal)
 end
 
 function fish_right_prompt
   # Move right prompt back up a line after newline of left prompt
   tput sc; tput cuu1; tput cuf 2
 
-  echo (kubectl_status)
+  echo (kubectl_status)" "(openstack_status)
 
   tput rc
 end
