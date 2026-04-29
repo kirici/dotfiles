263527873e252124f8c8339891f692904c112cb6
kirici <74684021+kirici@users.noreply.github.com>
2026-04-29 08:31:20 +0200

    feat: add fish func mkcd

---
+++ dot_config/private_fish/functions/mkcd.fish
@@ -0,0 +1,7 @@
+function mkcd --argument-names dir --description "mkdir and cd into it"
+  if test (count $argv) -eq 0
+    echo "Usage: mkcd <directory>"
+    return 1
+  end
+  mkdir -p $argv[1]; and cd $argv[1]
+end
